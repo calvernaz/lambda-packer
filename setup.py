8ab969eab6eab7fa7c6261c55954f8ec714dc6e9
Cesar Alvernaz <cesar.alvernaz@gmail.com>
2024-09-20 23:34:17 +0100

    Bump version: 0.1.14 → 0.1.15

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambda_packer",
-    version='0.1.14',
+    version='0.1.15',
     packages=find_packages(),
     install_requires=[
         "Click",
